e942ad256376812857438e1c1f007d954d864cd6
Amirmasoud Abedi <codingwithenjoy@gmail.com>
2025-08-04 15:26:30 +0330

    Update script.js

--- script.js
+++ script.js
@@ -572,3 +572,11 @@ function closePanel() {
 btnAppearance.addEventListener("click", () => openPanel("appearance"));
 btnAbout.addEventListener("click", () => openPanel("about"));
 closePanelBtn.addEventListener("click", closePanel);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('SW registered:', reg.scope))
+      .catch(err => console.error('SW registration failed:', err));
+  });
+}
