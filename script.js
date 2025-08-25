1b35c6853c8080484e574d44f8e7889cc5273ddc
Amirmasoud <codingwithenjoy@gmail.com>
2025-08-25 22:51:16 +0330

    new features and improvements

--- script.js
+++ script.js
@@ -929,14 +929,3 @@ removePasswordBtn.addEventListener("click", () => {
     removePasswordBtn.classList.add("hidden");
   }
 });
-
-document.addEventListener(
-  "touchmove",
-  function (e) {
-    const scrollable = e.target.closest("body");
-    if (!scrollable) {
-      e.preventDefault();
-    }
-  },
-  { passive: false }
-);
